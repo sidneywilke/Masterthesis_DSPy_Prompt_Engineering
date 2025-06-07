8c1ba0fe7cb3d0a89d54364e3b7402754455da45
Sidney <mail@sidneywilke.com>
2025-06-07 17:04:33 +0200

    for laptop

--- NER_using_DSPy.py
+++ NER_using_DSPy.py
@@ -26,7 +26,7 @@ if logger.hasHandlers():
 formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
 
 # Set up a file handler
-file_handler = logging.FileHandler('output_neu.log', mode='a', encoding='utf-8')
+file_handler = logging.FileHandler('output_neu_final.log', mode='a', encoding='utf-8')
 file_handler.setLevel(logging.INFO)
 file_handler.setFormatter(formatter)
 
@@ -64,6 +64,9 @@ class LoggerWriter:
 # Redirect standard output and error to the logger.
 sys.stdout = LoggerWriter(logger.info)
 sys.stderr = LoggerWriter(logger.error)
+
+
+
 
 
 def extract_people_entities(data_row: Dict[str, Any]) -> List[str]:
@@ -113,9 +116,6 @@ test_set = prepare_dataset(dataset["test"])
 
 
 
-
-
-
 class PeopleExtraction(dspy.Signature):
     """
     Extract contiguous tokens referring to specific people, if any, from a list of string tokens.
@@ -127,31 +127,33 @@ class PeopleExtraction(dspy.Signature):
 
 import os
 
-llm= os.getenv("TINY_LLM")
-api=os.getenv("LOCAL_API")
+llm= os.getenv("LARGE_LLM")
+api=os.getenv("SERVER_API")
 
 print("---------------------------------")
 print(llm+"  "+api+" Runde 1")
 
 people_extractor = dspy.ChainOfThought(PeopleExtraction)
-lm = dspy.LM(llm, api_base=api, api_key='')
+lm = dspy.LM(llm, api_base=api, api_key='', cache=False)
 dspy.configure(lm=lm)
 
 
-#Calculate precision
+
 def precision(tp: int, fp: int) -> float:
     # Handle division by zero
     return 0.0 if tp + fp == 0 else tp / (tp + fp)
 
-#Calculate recall
+
 def recall(tp: int, fn: int) -> float:
     return 0.0 if tp + fn == 0 else tp / (tp + fn)
 
-#Calculate F1-Score
+
 def f1_score(tp: int, fp: int, fn: int) -> float:
     prec = precision(tp, fp)
     rec = recall(tp, fn)
     return 0.0 if prec + rec == 0 else 2 * (prec * rec) / (prec + rec)
+
+
 
 def extraction_correctness_metric(example: dspy.Example, prediction: dspy.Prediction, trace=None) -> float:
     """
@@ -181,8 +183,7 @@ def extraction_correctness_metric(example: dspy.Example, prediction: dspy.Predic
     return f1_score(tp, fp, fn)
 
 
-print("---------------------------------------------------")
-print("-----------------------Baseline--------------------")
+
 evaluate_correctness = dspy.Evaluate(
     devset=test_set,
     metric=extraction_correctness_metric,
@@ -192,44 +193,52 @@ evaluate_correctness = dspy.Evaluate(
     return_outputs=True
 )
 
-#Evaluate the F1-score on the test set
+print("---------------------------------------------------")
+print("-----------------------Baseline--------------------")
+
+
 evaluate_correctness(people_extractor, devset=test_set, return_outputs=True)
 
 
 print("---------------------------------------------------")
 print("-----------------------LabeledFewShot--------------")
 
-# 3. Create the teleprompter
-fewshot_optimzer = LabeledFewShot(k=5)
 
-# 4. Compile (i.e., inject your 5-shot examples)
+fewshot_optimzer = LabeledFewShot(k=4)
+
+
 fewshot_optimzer_compile = fewshot_optimzer.compile(people_extractor, trainset=train_set)
 
 evaluate_correctness(fewshot_optimzer_compile, devset=test_set)
 
 dspy.inspect_history(n=1)
 
+
+
 print("---------------------------------------------------")
 print("-----------------------KNNFewShot------------------")
 from sentence_transformers import SentenceTransformer
 
-# Initialize KNNFewShot with a sentence transformer model
+
 knn_fewshot_optimizer = KNNFewShot(
-    k=3,
+    k=4,
     trainset=train_set,
     vectorizer=dspy.Embedder(SentenceTransformer("all-MiniLM-L6-v2").encode)
 )
 
-# Compile the QA module with few-shot learning
+
 knn_fewshot_optimizer_compile = knn_fewshot_optimizer.compile(people_extractor)
 
-# Use the compiled module
+
 evaluate_correctness(knn_fewshot_optimizer_compile, devset=test_set)
 
 dspy.inspect_history(n=1)
+
+
+
 print("---------------------------------------------------")
 print("-----------------------MIPROv2----------------------")
-# MIPROv2 optimization
+
 mipro_optimizer = dspy.MIPROv2(
     metric=extraction_correctness_metric,
     max_labeled_demos=4,
@@ -247,14 +256,16 @@ mipro_optimized_people_extractor = mipro_optimizer.compile(
     requires_permission_to_run=False,
 )
 
-# 5. Inspect best instruction
+
 evaluate_correctness(mipro_optimized_people_extractor, devset=test_set)
 dspy.inspect_history(n=1)
+
+
 
 print("---------------------------------------------------")
 print("-----------------------COPRO----------------------")
 
-# 3. Initialize COPRO
+
 copro_optimizer = COPRO(
     prompt_model=lm,
     metric=extraction_correctness_metric,
@@ -264,7 +275,7 @@ copro_optimizer = COPRO(
     track_stats=True,
 )
 
-# 4. Compile (optimize) your program
+
 with dspy.context(num_completions=1):
     copro_optimizer_compiled = copro_optimizer.compile(
         student=people_extractor,
@@ -273,7 +284,7 @@ with dspy.context(num_completions=1):
         eval_kwargs=dict(num_threads=8)
 )
 
-# 5. Inspect best instruction
+
 evaluate_correctness(copro_optimizer_compiled, devset=test_set)
 dspy.inspect_history(n=1)
 
@@ -282,12 +293,12 @@ dspy.inspect_history(n=1)
 print("---------------------------------------------------")
 print("-----------------------BootstrapFewShot------------")
 
-#BootstrapFewShot optimization
+
 from dspy.teleprompt import BootstrapFewShot
 bootstrap_fewshot_optimizer= BootstrapFewShot(
     metric=extraction_correctness_metric,
     max_bootstrapped_demos=4,
-    max_labeled_demos=16,
+    max_labeled_demos=4,
     max_rounds=1,
     max_errors=5,
     teacher_settings=dict(lm=lm)
@@ -295,14 +306,16 @@ bootstrap_fewshot_optimizer= BootstrapFewShot(
 
 bootstrap_fewshot_optimizer_compiled=bootstrap_fewshot_optimizer.compile(people_extractor, trainset=train_set)
 
-#Evaluate the F1-score with the bootstrapped optimizer on the test set
+
 evaluate_correctness(bootstrap_fewshot_optimizer_compiled, devset=test_set)
 
-#Inspect the system prompts to examine the evolution of the system prompts after the optimization process
+
 dspy.inspect_history(n=1)
 
+
+
 print("---------------------------------------------------")
-print("-----------------------BootstrapFewShotWithRandomSearch------------")
+print("-------BootstrapFewShotWithRandomSearch------------")
 
 #BootstrapFewShot optimization
 from dspy.teleprompt import BootstrapFewShotWithRandomSearch
@@ -314,20 +327,10 @@ bootstrap_fewshot_withrandom_optimizer = BootstrapFewShotWithRandomSearch(
 
 bootstrap_fewshot_withrandom_optimizer_compiled=bootstrap_fewshot_withrandom_optimizer.compile(people_extractor, trainset=train_set)
 
-#Evaluate the F1-score with the bootstrapped optimizer on the test set
+
 evaluate_correctness(bootstrap_fewshot_withrandom_optimizer_compiled, devset=test_set)
 
-#Inspect the system prompts to examine the evolution of the system prompts after the optimization process
+
 dspy.inspect_history(n=1)
 
-'''
-print("---------------------------------------------------")
-print("-----------------------BootstrapFinetune----------------------")
-# Optimize via BootstrapFinetune.
-dspy.settings.experimental = True
-bootstrap_finetune_optimizer = dspy.BootstrapFinetune(metric=extraction_correctness_metric, num_threads=8)
-bootstrap_finetune_optimized = bootstrap_finetune_optimizer.compile(people_extractor, trainset=train_set)
 
-evaluate_correctness(bootstrap_finetune_optimized, devset=test_set)
-
-dspy.inspect_history(n=1)'''
